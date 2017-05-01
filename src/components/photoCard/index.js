67736dacd37ecba014fb76b54674bb7cc2c5db6e
Clinton Nguyen <clinton.ngyn@gmail.com>
2017-05-01 10:02:10 -0400

    Done with photo card for now

--- src/components/photoCard/index.js
+++ src/components/photoCard/index.js
@@ -7,17 +7,46 @@ export default class PhotoCard extends Component {
     this.state = {
       height: props.exif.height,
       width: props.exif.width,
-      portfolio_url: props.exif.user.portfolio_url,
+      portfolioURL: props.exif.user.portfolio_url,
       profile: props.exif.links.html,
       fullView: props.exif.urls.full,
       thumb: props.exif.urls.small,
       url: props.exif.links.html,
-      showDetails: false
+      author: props.exif.user.name,
+      primaryColor: props.exif.color,
+      location: props.exif.location,
+      showDetails: false,
+      showModal: false
     }
+    this.toggleDetails = this.toggleDetails.bind(this)
+    this.dismissModal = this.dismissModal.bind(this)
+    this.handleKeydown = this.handleKeydown.bind(this)
   }
 
-  showHover() {
-    console.log("Showing hover!")
+  showModal(e) {
+    e.preventDefault()
+    document.addEventListener("keydown", this.handleKeydown)
+    this.setState({...this.state, showModal: true})
+  }
+
+  handleKeydown(e) {
+    if (e.key === "Escape") this.dismissModal()
+  }
+
+  dismissModal() {
+    document.removeEventListener("keydown", this.handleKeydown)
+    this.setState({...this.state, showModal: false})
+  }
+
+  renderModal() {
+    return (
+      <div class={style.photo_modal}>
+        <div class={style.photo_modal__photo_container}>
+          <button class={style.photo_modal__close} onclick={this.dismissModal}>✕</button>
+          <div class={style.photo_modal__photo}><img src={this.state.fullView} class={style.photo_modal__img} /></div>
+        </div>
+      </div>
+    )
   }
 
   toggleDetails() {
@@ -25,13 +54,16 @@ export default class PhotoCard extends Component {
   }
 
   render() {
-    console.log(this.state)
-    // TODO: change visibility hidden to opacity 0 and toggle from there
     return (
-      <div class={style.photocard2} style={{ background: `url(${this.state.thumb})`}} onmouseover={this.toggleDetails.bind(this)}>
-        <div class={this.state.showDetails ? "hoverHidden hoverShown" : "hoverHidden"} onmouseout={this.toggleDetails.bind(this)}><a href="#">Good</a></div>
+      <div>{ this.state.showModal ? this.renderModal() : null }
+      <a href="#" title="Click to see a bigger view" onclick={this.showModal.bind(this)}>
+      <div class={style.photocard2} style={{ background: `url(${this.state.thumb})`}}>
+        <div class={style.photocard__details} onmouseout={this.toggleDetails} onmouseover={this.toggleDetails} style={this.state.showDetails ? { opacity: 1, transition: "0.3s all ease-in" } : { opacity: 0, transition: "0.3s all ease-out" }}>
+          <a href={this.state.profile}> By {this.state.author}</a>
+        </div>
         <img src={this.state.thumb} style={{visibility: "hidden"}}/>
-      </div>
+      </div></a>
+    </div>
     )
   }
 }
